67c37c66778dc2834b3a432fdb5de5e9ff114ba8
ester <esterwanderley@hotmail.com>
2023-10-06 15:26:55 -0300

    first commit

---
+++ app/controllers/market_lists_controller.rb
@@ -0,0 +1,2 @@
+class MarketListsController < ApplicationController
+end
---
+++ test/controllers/market_lists_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class MarketListsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
