66d6cb218e7041594f5d455f9f03c2f58a4b0756
ester <esterwanderley@hotmail.com>
2023-10-23 17:58:23 -0300

    itens related

---
+++ app/controllers/market_items_controller.rb
@@ -0,0 +1,21 @@
+class MarketItemsController < ApplicationController
+    def new
+        # lógica para 'new'
+    end
+    
+    def create
+        # lógica para 'create'
+    end
+    
+    def edit
+        # lógica para 'edit'
+    end
+    
+    def update
+        # lógica para 'update'
+    end
+    
+    def destroy
+        # lógica para 'destroy'
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
-  Rails.application.routes.draw do
-    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-    resources :market_lists
-    root to: 'market_lists#index'
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :market_lists do
+    resources :market_items
   end
+
+  root to: 'market_lists#index'
+end
+
 
