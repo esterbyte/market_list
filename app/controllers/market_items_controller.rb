29f74e01301bc2e67942a6c7e90d7b1ce97eca07
ester <esterwanderley@hotmail.com>
2023-10-31 18:27:56 -0300

    flash messages

--- app/controllers/market_items_controller.rb
+++ app/controllers/market_items_controller.rb
@@ -1,36 +1,41 @@
 class MarketItemsController < ApplicationController
 
-    def new
-      @market_list = MarketList.find(params[:market_list_id])
-      @market_item = MarketItem.new
-    end
-    
-    def create
-      @market_list = MarketList.find(params[:market_list_id])
-      @market_item = @market_list.market_items.new(market_item_params)
 
-      if @market_item.save
-        flash[:success] = 'Item de mercado criado com sucesso.'
-        redirect_to @market_list
-      else
-        flash[:error] = 'Erro ao criar o item de mercado.'
-        render :new
-      end
+  def new
+    @market_list = MarketList.find(params[:market_list_id])
+    @market_item = MarketItem.new
+  end
+  
+  def create
+    @market_list = MarketList.find(params[:market_list_id])
+    @market_item = @market_list.market_items.new(market_item_params)
+
+    if @market_item.save
+      flash[:success] = 'Item de mercado criado com sucesso.'     
+    else
+      flash[:error] = 'Erro ao criar o item de mercado.'
     end
+
+    redirect_to @market_list
+  end
+  
+  def destroy
+    @market_item = MarketItem.find(params[:id])
     
-    def destroy
-      @market_item = MarketItem.find(params[:id])
-      @market_item.destroy
+    if @market_item.destroy
       flash[:success] = 'Item de mercado excluído com sucesso.'
-      redirect_to @market_item.market_list
+    else
+      flash[:error] = 'Erro ao excluir o item de mercado.'
     end
+    
+    redirect_to @market_item.market_list
 
-    def show
-      @market_list = MarketList.find(params[:market_list_id])
-      @market_item = @market_list.market_items.find(params[:id])
-    end
+  def show
+    @market_list = MarketList.find(params[:market_list_id])
+    @market_item = @market_list.market_items.find(params[:id])
+  end
 
-    private
+  private
 
   def market_item_params
     params.require(:market_item).permit(:name, :quantidade)
--- app/controllers/market_lists_controller.rb
+++ app/controllers/market_lists_controller.rb
@@ -1,48 +1,54 @@
 class MarketListsController < ApplicationController
-    def index
-      @market_lists = MarketList.all
-    end 
-    
-    def show
+  
+  def index
+    @market_lists = MarketList.all
+  end 
+  
+  def show
     @market_list = MarketList.find(params[:id])
-    end  
+  end  
 
-    def create
-      @market_list = MarketList.new(params.require(:market_list).permit(:name, :market_date))
-  
-      if @market_list.save
-        flash[:success] = 'Lista criada com sucesso'
-        redirect_to action: :index
-      else
-        flash[:error] = 'Erro ao criar a lista'
-        render :new
-      end
-    end
+  def create
+    @market_list = MarketList.new(params.require(:market_list).permit(:name, :market_date))
 
-    def update
-      @market_list = MarketList.find(params[:id])
-      @market_list.attributes = params.require(:market_list).permit(:name, :market_date)
-  
-      if @market_list.save
-        flash[:success] = 'Lista editada com Sucesso'
-        redirect_to action: :index
-      else
-        flash[:error] = 'Erro ao editar a lista'
-        render :edit
-      end
+    if @market_list.save
+      flash[:success] = 'Lista criada com sucesso'
+    else
+      flash[:error] = 'Erro ao criar a lista'
     end
-
-    def destroy
-      @market_list = MarketList.find(params[:id])
-      @market_list.destroy
-      redirect_to market_lists_path
-    end
-
-    def new
-      @market_list = MarketList.new
-    end
-
-    def edit
-      @market_list = MarketList.find(params[:id])
-    end
+    redirect_to action: :index
   end
+
+  def update
+    @market_list = MarketList.find(params[:id])
+    @market_list.attributes = params.require(:market_list).permit(:name, :market_date)
+
+    if @market_list.save
+      flash[:success] = 'Lista editada com Sucesso'
+    else
+      flash[:error] = 'Erro ao editar a lista'
+      render :edit
+    end
+    redirect_to action: :index
+  end
+
+  def destroy
+    @market_list = MarketList.find(params[:id])
+    
+    if @market_list.destroy
+      flash[:success] = 'Lista de mercado excluída com sucesso.'
+    else
+      flash[:error] = 'Erro ao excluir a lista de mercado.'
+    end
+    redirect_to market_lists_path
+  end
+  
+
+  def new
+    @market_list = MarketList.new
+  end
+
+  def edit
+    @market_list = MarketList.find(params[:id])
+  end
+end
