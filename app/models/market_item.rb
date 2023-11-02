0d599319a2e9d0758c8e5ebe875ce6ba8d18ab0d
ester <esterwanderley@hotmail.com>
2023-11-02 15:54:41 -0300

    change addressed - pedro

--- app/models/market_item.rb
+++ app/models/market_item.rb
@@ -1,5 +1,5 @@
 class MarketItem < ApplicationRecord
   belongs_to :market_list
   validates :name, presence: true, length: { minimum: 5 }
-  validates :quantidade, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :quantidade, presence: true, numericality: { only_integer: true, greater_than: 0 }
 end
