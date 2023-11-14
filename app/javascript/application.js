ab37f44e89077e3b59acc0b0ea4789960731f1dd
ester <esterwanderley@hotmail.com>
2023-11-14 16:41:20 -0300

    customizacao de botao e funcionalidade dismiss

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,18 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+import "bootstrap";
 
-//= require jquery
-//= require jquery_ujs
+document.addEventListener("turbo:load", function () {
+  // Fechar a notificação quando o botão de "dismiss" for clicado
+  const closeButton = document.querySelector(".alert-dismissible .close");
+  if (closeButton) {
+    closeButton.addEventListener("click", function () {
+      const alert = this.closest(".alert-dismissible");
+      if (alert) {
+        alert.remove();
+      }
+    });
+  }
+});
+
