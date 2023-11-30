bb61f093443d0a4532c16be06b8f0a98a4757d19
ester <esterwanderley@hotmail.com>
2023-11-30 14:58:29 -0300

    css ajustes

--- app/models/market_item.rb
+++ app/models/market_item.rb
@@ -1,5 +1,5 @@
 class MarketItem < ApplicationRecord
   belongs_to :market_list
-  validates :name, presence: true, length: { minimum: 5 }
+  validates :name, presence: true, length: { minimum: 2 }
   validates :quantidade, presence: true, numericality: { only_integer: true, greater_than: 0 }
 end
