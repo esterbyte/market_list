52d603ae756bfe9652b52d4af21a129857252ac8
ester <esterwanderley@hotmail.com>
2023-12-06 18:06:20 -0300

    index market_lists_controller_test.rb

--- test/controllers/market_lists_controller_test.rb
+++ test/controllers/market_lists_controller_test.rb
@@ -1,7 +1,16 @@
-require "test_helper"
+require 'test_helper'
 
-class MarketListsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+class MarketListsControllerTest < ActionController::TestCase
+  test "índice atribui market_lists ordenado por market_date" do
+    market_list1 = MarketList.create(market_date: Date.today)
+    market_list2 = MarketList.create(market_date: Date.yesterday)
+
+    get :index
+
+    assert_response :success
+
+    assert_not_nil assigns(:market_lists)
+    assert_equal [market_list2, market_list1], assigns(:market_lists)
+  end
 end
+
