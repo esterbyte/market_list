e5c6c9a46af6863f33c5d046ef5877fe62a98609
ester <esterwanderley@hotmail.com>
2023-12-21 14:06:05 -0300

    tentativas de fix e unit test

--- test/controllers/market_lists_controller_test.rb
+++ test/controllers/market_lists_controller_test.rb
@@ -1,7 +1,17 @@
-require "test_helper"
+require 'test_helper'
 
-class MarketListsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
+class MarketListsControllerTest < ActiveSupport::TestCase
+  setup do
+    @market_list = market_lists(:one)
+  end
+
+  # test "should get index" do
+  #   get :index
+  #   assert_response :success
   # end
+
+  test "should get show" do
+    get :show, params: { id: @market_list.id }
+    assert_response :success
+  end
 end
